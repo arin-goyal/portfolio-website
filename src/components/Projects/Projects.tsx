02a45a5ebcf5173c9bcad2e87f89773bf06923f7
arin-goyal <aringoyalper@gmail.com>
2026-04-07 20:14:44 +0530

    fixed show more button link in dev mode

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -40,7 +40,7 @@ const Projects = ({ isDevMode }: ProjectsProps) => {
 
         {/* Show More Button */}
         <div className="projects-footer">
-          <a href="https://www.behance.net/aringoyal" target="_blank" rel="noreferrer" className={`show-more-btn ${isDevMode ? 'dev' : ''}`}>
+          <a href={isDevMode ? "https://github.com/arin-goyal" : "https://www.behance.net/aringoyal"} target="_blank" rel="noreferrer" className={`show-more-btn ${isDevMode ? 'dev' : ''}`}>
             Show More
             <svg className="btn-arrow" xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
               <path d="M0.292893 10.8995C-0.0976311 11.29 -0.0976311 11.9232 0.292893 12.3137C0.683418 12.7042 1.31658 12.7042 1.70711 12.3137L1 11.6066L0.292893 10.8995ZM12.6066 1C12.6066 0.447715 12.1589 -8.19855e-08 11.6066 -3.34867e-07L2.6066 -2.08426e-07C2.05432 -5.45601e-07 1.6066 0.447715 1.6066 1C1.6066 1.55228 2.05432 2 2.6066 2L10.6066 2L10.6066 10C10.6066 10.5523 11.0543 11 11.6066 11C12.1589 11 12.6066 10.5523 12.6066 10L12.6066 1ZM1 11.6066L1.70711 12.3137L12.3137 1.70711L11.6066 1L10.8995 0.292893L0.292893 10.8995L1 11.6066Z" fill="currentColor"/>
